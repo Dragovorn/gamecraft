c00fe4e0e83115639de617a5f1496caead0d35c4
y0op <camjanssen120@gmail.com>
2017-12-07 10:22:13 -0800

    Added teams

--- src/main/java/com/dragovorn/gamecraft/player/GamePlayer.java
+++ src/main/java/com/dragovorn/gamecraft/player/GamePlayer.java
@@ -3,12 +3,16 @@ package com.dragovorn.gamecraft.player;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
+import java.util.UUID;
+
 public class GamePlayer {
 
     private Player player;
+    private UUID uuid;
 
     GamePlayer(Player player) {
         this.player = player;
+        this.uuid = player.getUniqueId();
     }
 
     public void save() {
