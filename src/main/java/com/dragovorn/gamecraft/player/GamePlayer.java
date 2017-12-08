22b4444f23b6f6634ca0eab06fa27a02415e3972
y0op <camjanssen120@gmail.com>
2017-12-08 09:12:33 -0800

    override equals in player

--- src/main/java/com/dragovorn/gamecraft/player/GamePlayer.java
+++ src/main/java/com/dragovorn/gamecraft/player/GamePlayer.java
@@ -22,4 +22,22 @@ public class GamePlayer {
     public void sendMessage(String message) {
         this.player.sendMessage(ChatColor.translateAlternateColorCodes('&', message));
     }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        if (obj == null) {
+            return false;
+        }
+
+        if (obj instanceof GamePlayer) {
+            return ((GamePlayer) obj).getUuid().equals(this.uuid);
+        }
+
+        return obj instanceof Player && obj.equals(player);
+    }
 }
