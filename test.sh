54072ee6d1feb0c3fa04ec4ce61ce18cc68137dc
Dragovorn <me@dragovorn.com>
2017-12-09 01:00:15 -0500

    clean up the script

--- test.sh
+++ test.sh
@@ -5,10 +5,16 @@ mvn clean package shade:shade
 STATUS=$?
 
 if [ ${STATUS} -eq 0 ]; then
-    echo "BUILD SUCCESSFUL, LAUNCHING TEST ENVIRONMENT"
-    mv target/GameCraft.jar test-server/plugins/
-    cd test-server
-    java -jar server.jar
+    echo "BUILD SUCCESSFUL!"
+
+    if [ -d "server/" ]; then
+        echo "SERVER DIRECTORY FOUND! LAUNCHING SERVER!"
+        mv target/GameCraft.jar server/plugins/
+        cd server
+        java -jar server.jar
+    else
+        echo "NO SERVER DIRECTORY DETECTED! NOT LAUNCHING SERVER!"
+    fi
 else
     echo "BUILD FAILED!"
 fi
