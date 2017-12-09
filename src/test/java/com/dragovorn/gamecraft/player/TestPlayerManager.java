beb4e8241a1d08efba5abb668053378d4e9b001d
Dragovorn <me@dragovorn.com>
2017-12-09 03:21:06 -0500

    write some tests

--- src/test/java/com/dragovorn/gamecraft/player/TestPlayerManager.java
+++ src/test/java/com/dragovorn/gamecraft/player/TestPlayerManager.java
@@ -3,6 +3,7 @@ package com.dragovorn.gamecraft.player;
 import com.dragovorn.gamecraft.Main;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,7 +16,7 @@ import static org.junit.Assert.assertEquals;
 import static org.powermock.api.mockito.PowerMockito.mock;
 import static org.powermock.api.mockito.PowerMockito.when;
 
-@PrepareForTest({Main.class, PlayerManager.class, Player.class, PlayerJoinEvent.class})
+@PrepareForTest({Main.class, PlayerManager.class, Player.class, PlayerJoinEvent.class, PlayerQuitEvent.class})
 @RunWith(PowerMockRunner.class)
 public class TestPlayerManager {
 
@@ -36,6 +37,28 @@ public class TestPlayerManager {
         this.manager.setPlayerClass(TestPlayer.class);
 
         assertEquals(TestPlayer.class, this.manager.getPlayer(registerPlayer()).getClass());
+    }
+
+    @Test
+    public void testDeregisterPlayerNoType() {
+        Player player = registerPlayer();
+
+        PlayerQuitEvent event = mock(PlayerQuitEvent.class);
+        when(event.getPlayer()).thenReturn(player);
+
+        this.manager.deregisterPlayer(event);
+    }
+
+    @Test
+    public void testDeregisterPlayerCustomType() {
+        this.manager.setPlayerClass(TestPlayer.class);
+
+        Player player = registerPlayer();
+
+        PlayerQuitEvent event = mock(PlayerQuitEvent.class);
+        when(event.getPlayer()).thenReturn(player);
+
+        this.manager.deregisterPlayer(event);
     }
 
     private Player registerPlayer() {
